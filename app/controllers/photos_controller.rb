8fc0befe93d7a9b0d1a4957a472207e697dc6f5f
Taro Minowa Higepon <higepon@gmail.com>
2013-07-08 08:12:15 -0700

    Upload with json

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,20 @@
 class PhotosController < ApplicationController
+  respond_to :html, :json
 
   def index
     @photos = Photo.all
+    respond_with @photos
   end
 
   def new
     @photo = Photo.new
   end
 
+  # % curl -F "photo[photo_image]=@/Users/higepon/Desktop/a.jpg" http://localhost:3000/photos.json
   def create
     @photo = Photo.new(params[:photo])
     @photo.save
+    respond_with @photo
   end
 
 end
