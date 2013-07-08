2f53195d95396f0bccc2c131c792f0a9fe0a575c
Taro Minowa Higepon <higepon@gmail.com>
2013-07-08 16:11:06 -0700

    Generated deck model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708011045) do
+ActiveRecord::Schema.define(:version => 20130708230919) do
+
+  create_table "decks", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "photos", :force => true do |t|
     t.string   "name"
