31cf95e76c22147894d8cae1c4661da33bcad284
Taro Minowa Higepon <higepon@gmail.com>
2013-07-07 18:03:26 -0700

    Initial rails new dochi_api.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DochiApi::Application.config.secret_token = '55f79b4a44fda44ac57f1420def7d7cb46b7063e57fd1ace45f364c29d5f2b513b114384b762825ada1c208005c361baf1a708aa8eac8ad6bcff16e183428064'
