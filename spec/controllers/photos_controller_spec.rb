9c55339e294e5309b551dae8807301a513fedea4
Taro Minowa Higepon <higepon@gmail.com>
2013-07-08 15:53:31 -0700

    Added test for json.

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 
 describe PhotosController do
 
+  before do
+    @file = fixture_file_upload('/keiko.jpg', 'image/jpg')
+  end
+
   it "should get index" do
     request.accept = "application/json"
     get 'index'
     response.should be_success
   end
 
+  it "can upload a license" do
+    request.accept = "application/json"
+    post :create, :upload => @file
+    response.should be_success
+  end
 end
