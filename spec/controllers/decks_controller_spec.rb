968e1854b531b1c58439e67375ee6987512125eb
Taro Minowa Higepon <higepon@gmail.com>
2013-07-08 16:16:42 -0700

    (Red) Added deck spec.

--- spec/controllers/decks_controller_spec.rb
+++ spec/controllers/decks_controller_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe DecksController do
 
+  it "create deck" do
+    request.accept = "application/json"
+    post :create
+    response.should be_success
+  end
+
 end
