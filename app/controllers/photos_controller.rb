df526a6e3d9421cb71961d3f4d874dbab0a3a312
Taro Minowa Higepon <higepon@gmail.com>
2013-07-09 16:08:31 -0700

    upload photo from iOS works.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,7 +15,12 @@ class PhotosController < ApplicationController
 
   # % curl -F "photo[photo_image]=@/Users/higepon/Desktop/a.jpg" http://localhost:3000/photos.json
   def create
+    pp 'here'
+    pp params
+    pp "here2"
+    pp params[:photo]
     @photo = Photo.new(params[:photo])
+    pp "we here"
     @photo.save
     pp @photo
     respond_with @photo
