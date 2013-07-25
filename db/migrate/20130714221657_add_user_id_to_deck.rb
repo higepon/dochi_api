bb6787ec804abd19cf983cbf604e0966060fdc0f
Taro Minowa Higepon <higepon@gmail.com>
2013-07-24 21:01:42 -0700

    Added user model.

---
+++ db/migrate/20130714221657_add_user_id_to_deck.rb
@@ -0,0 +1,5 @@
+class AddUserIdToDeck < ActiveRecord::Migration
+  def change
+    add_column :decks, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130714221657) do
+ActiveRecord::Schema.define(:version => 20130725035914) do
 
   create_table "decks", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(:version => 20130714221657) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.integer  "deck_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
