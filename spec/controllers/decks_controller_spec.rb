de9b08eeec38c590df0292c3afd9cf2b3ae9138c
Taro Minowa Higepon <higepon@gmail.com>
2013-07-24 21:03:57 -0700

    (Red) Added a test for user name.

--- spec/controllers/decks_controller_spec.rb
+++ spec/controllers/decks_controller_spec.rb
@@ -15,6 +15,7 @@ describe DecksController do
     response.should be_success
     response.body.should have_json_type(Array).at_path("0/photos")
     response.body.should have_json_type(Integer).at_path("0/user_id")
+    response.body.should have_json_type(String).at_path("0/user_name")
     response.body.should have_json_type(String).at_path("0/photos/0/url")
   end
 
