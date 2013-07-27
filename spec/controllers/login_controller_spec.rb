1d54876fc5a8f1e5b21680cd79d90a564a5fd1e6
Taro Minowa Higepon <higepon@gmail.com>
2013-07-27 14:16:29 -0700

    (red) added test for login facebook

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe LoginController do
 
+  it "facebook returns facebook user object" do
+    request.accept = "application/json"
+    post :facebook
+    response.should be_success
+    response.body.should have_json_path("name")
+  end
+
 end
