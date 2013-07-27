ea2f43f9d07fc934f5ffc6bd733045c273a3b7f0
Taro Minowa Higepon <higepon@gmail.com>
2013-07-27 13:17:42 -0700

    (red) Added a test for fb_user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe User do
+  it "fetch facebook user object" do
+    fb_user = User.fb_user_by_token("CAAJPeH9eTKIBAFZAE4IdDdDY1S0msZAqwcRo4iw7IZBGVRZAvlD8d5HV7gTxAOSVatfnLZAEoRsMNHxG005gHFpkWeSBne5RfgnhJHUhtKKA7cNbXbN85ZAFAXQ2k7rNoHsfZCpQUeiUuoBJ8VLkkQZC6yqXGu1XLD4ZC9vXy7aQx40xR8VCrIP0I7JwlCYLSZBpvwPNkXipprPL4kwqnjePTzPgxJASovQeQZD")
+    fb_user[:email].should_not be_nil
+    fb_user[:name].should_not be_nil
+  end
 end
