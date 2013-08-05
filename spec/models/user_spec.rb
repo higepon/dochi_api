f9efb2a80ac64f86d719cc309ba102050501a9ad
Taro Minowa Higepon <higepon@gmail.com>
2013-08-04 20:41:21 -0700

    (Red) Added a test for like! if it already liked.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe User do
+  fixtures :likes, :decks
   describe "#like!" do
     it "creates & returns new like model if it doesn't exist" do
-      deck = Deck.first
+      not_liked_yet_deck_id = 3
+      deck = Deck.find(not_liked_yet_deck_id)
       deck.should_not be_nil
       
       user = User.first
@@ -14,7 +16,15 @@ describe User do
       like.deck_id.should == deck.id
     end
 
-    it "doesn't nothing it exists" do
+    it "doesn't nothing if it exists" do
+      liked_deck_id = 1
+      deck = Deck.find(liked_deck_id)
+      deck.should_not be_nil
+      
+      user = User.find(1234)
+      user.should_not be_nil
+
+      user.like!(deck).should be_nil
     end
 
     it "raise error if deck is not specified" do
