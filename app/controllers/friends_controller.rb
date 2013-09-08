f1d9c347045de23c681fce8b6c9f7d0b72f21682
Taro Minowa Higepon <higepon@gmail.com>
2013-09-08 16:32:12 -0700

    simplify.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,18 +2,8 @@ class FriendsController < ApplicationController
   respond_to :json
   before_filter :_login
 
+  # :location is workaround.
   def show
-    respond_with(@user, :only => [:id, :name, :avatar_url], :include => [{:friends => {:only => [:id, :name, :avatar_url]}}], :location => '/hoge')
+    respond_with(@user, :only => [:id, :name, :avatar_url], :include => [{:friends => {:only => [:id, :name, :avatar_url]}}], :location => '/')
   end
-  # rescue => e
-  #   puts e
-  #   render json: { :status => :error }, :status => :bad_request, :location => "hoge"
-  # end
-
-  def create
-  end
-
-  def update
-  end
-
 end
