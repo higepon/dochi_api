bfa91da8c2b6927502eafe4843880b18c4886d57
Taro Minowa Higepon <higepon@gmail.com>
2013-10-24 18:38:31 -0700

    imagemagick append works, but ugly.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,5 +1,5 @@
 require 'dragonfly/rails/images'
-
+require 'pp'
 require 'dragonfly'
 app = Dragonfly[:images]
 
@@ -14,5 +14,87 @@ if Rails.env.production?
     )
   end
 end
+
+class MyProcessor
+
+  def two(temp_object, opts={})
+    puts "two"
+  end
+
+
+  private
+
+  def my_helper_method
+    # do stuff
+  end
+
+end
+
+include Dragonfly::ImageMagick::Utils
+
+app.processor.add :watermark do |temp_object, *args|
+begin
+  # If exact same path, unknown stack overflow is issued in Shell.rb around "if log_commands"
+  if false #temp_object.path == args[0].path
+    temp_object
+  else
+  pp args
+  # use temp_object.data, temp_object.path, temp_object.file, etc.
+#  app.configure {|c| c.log_commands = false }
+  tempfile = new_tempfile(nil)
+#    puts `ls -la #{tempfile.path}`
+#  puts "convert -background blue +append #{quote(temp_object.path)} #{quote(args[0].path)} #{quote(tempfile.path)}"
+  #run "convert","+append #{quote(temp_object.path)} #{quote(args[0].path)} #{quote(tempfile.path)}"
+
+    p0 = Photo.find(args[1]).photo_image
+    p1 = Photo.find(args[2]).photo_image
+    puts "p0 = #{p0} p1=#{p1}"
+    result = `convert -background blue +append #{quote(p0.path)} #{quote(p1.path)} #{quote(tempfile.path)}`
+#    result = `convert -background blue +append #{quote(temp_object.path)} #{quote(args[0].path)} #{quote(tempfile.path)}`
+    puts `ls -la #{tempfile.path}`
+    pp $?.exitstatus
+  tempfile
+  end
+  rescue => e
+  STDERR.puts("ERRR=#{e}")
+  # return a String, Pathname, File or Tempfile
+end
+end
+
+#app.processor.register(MyProcessor)
+
+# # module Dragonfly
+# #   module ImageMagick
+#     class Processor2
+#       # include Configurable
+#       # include Shell
+
+#       def new_tempfile(ext=nil)
+#         tempfile = ext ? Tempfile.new(['dragonfly', ".#{ext}"]) : Tempfile.new('dragonfly')
+#         tempfile.binmode
+#         tempfile.close
+#         tempfile
+#       end
+
+
+#       def convert(path1, path2)
+#         tempfile = new_tempfile(nil)
+#         puts "convert","#{path1} -append #{path2} #{quote(tempfile.path)}"
+#         run "convert"," +append #{path1} #{path2} hoge.jpg"
+#         tempfile
+#         puts "hoge called" 
+#       end
+     
+#       def c(path1, path2)
+#         convert(path1, path2)
+#       end
+#     end
+# #  end
+# #end
+
+
+# app.configure do |c|
+#   c.processor.register(Processor2)
+# end
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
--- script/image_append.rb
+++ script/image_append.rb
@@ -1,20 +1,65 @@
 #!/usr/bin/env ruby
 require 'dragonfly'
 
-module Dragonfly
-  module ImageMagick
-    class Hoge
-      include Configurable
-      include Shell
-      def hoge(path1, path2)
-        puts "convert","#{path1} -append #{path2} hoge.jpg"
-        run "convert"," +append #{path1} #{path2} hoge.jpg"
-        puts "hoge called" 
-      end
-    end
-  end
-end
+p0 = Photo.all[2]
+p1 = Photo.all[4]
 
-h = Dragonfly::ImageMagick::Hoge.new
-h.hoge(Photo.first.photo_image.path, Photo.first.photo_image.path)
+image = Photo.all[2].photo_image
+image2 = Photo.all[4].photo_image
+
+# うまくうごかず convert コマンドがなんにも言わない場合がある
+# file exist で確認すべき
+new_image = image2.process(:watermark, nil, p0.id, p1.id)
+puts "HH"
+    puts `ls -la #{new_image.path}`
+result = `open #{new_image.thumb("30x40").path}`
+puts new_image.url
+puts new_image.thumb("30x40").path
+while (true) do
+end
+# module Dragonfly
+#   module ImageMagick
+#     class Processor
+#       # include Configurable
+#       # include Shell
+
+#       def new_tempfile(ext=nil)
+#         tempfile = ext ? Tempfile.new(['dragonfly', ".#{ext}"]) : Tempfile.new('dragonfly')
+#         tempfile.binmode
+#         tempfile.close
+#         tempfile
+#       end
+
+
+#       def convert(path1, path2)
+#         tempfile = new_tempfile(nil)
+#         puts "convert","#{path1} -append #{path2} #{quote(tempfile.path)}"
+#         run "convert"," +append #{path1} #{path2} hoge.jpg"
+#         tempfile
+#         puts "hoge called" 
+#       end
+     
+#       def c(path1, path2)
+#         convert(path1, path2)
+#       end
+#     end
+#   end
+# end
+
+# h = Dragonfly::ImageMagick::Processor.new
+# puts h.c(Photo.first.photo_image.path, Photo.first.photo_image.path)
+
+# class MyProxy
+#   include Dragonfly::Shell
+#   def path
+#     "-append #{Photo.first.photo_image.path} #{Photo.first.photo_image.path}"
+#   end
+# end
+
+# Dragonfly::app.config.processor.register(ImageMagick::Processor)
+# image2 = Photo.all[1].photo_image
+# puts image1.raw_identify("-append", image1.path, image2.path).path
+
+#Dragonfly::ImageMagick::Utils.run convert_command, MyProxy.new
+
 #Dragonfly::ImageMagick.hoge(Photo.first.photo_image.path, Photo.first.photo_image.path)
