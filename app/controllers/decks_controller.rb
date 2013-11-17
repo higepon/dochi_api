296bb895f805fe53135763714e0a8a1afcf1ed74
Taro Minowa Higepon <higepon@gmail.com>
2013-11-17 12:25:42 -0800

    (Green) Implemented API for Deck#is_public.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -44,6 +44,7 @@ class DecksController < ApplicationController
     @deck = Deck.new
     @deck.user_id = @user.id
     @deck.url_key = SecureRandom.urlsafe_base64(nil, false)
+    @deck.is_public = (params[:is_public] == "1")
     @deck.save
     respond_with @deck
   end
