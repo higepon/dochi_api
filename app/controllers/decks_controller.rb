1fdb7d4d111458e994a5978b5ec7addf4df05cde
Taro Minowa Higepon <higepon@gmail.com>
2013-11-20 20:28:56 -0800

    d

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,5 @@
 class DecksController < ApplicationController
-  respond_to :html, :perma_link
+  respond_to :html
   respond_to :json, :except => :perma_link
   skip_before_filter  :verify_authenticity_token
   before_filter :_login, :except => [:perma_link]
